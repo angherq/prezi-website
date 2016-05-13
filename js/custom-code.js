567ebf7f56a20fdff1c675be796b5a9258a401f0
angherq <anruhe.q@gmail.com>
2016-05-12 19:23:25 -0500

    integrated with API responses

--- js/custom-code.js
+++ js/custom-code.js
@@ -1,36 +1,81 @@
 (function() {
     $.ajax({
-        url: "prezis.json",
+        url: "http://localhost:8080/prezis",
         dataType: "json",    
         context: document.body,
         success: function(data) {
             
-            $.each(data, function(i, item) {
+            if(data.status == 'success') {
                 
-                thumb  = '  <div class="col-md-4 portfolio-item">';
-                thumb += '      <a href="http://prezi.com/prezi/id/' + item.id + '">';
-                thumb += '         <img class="img-responsive" src="'+item.thumbnail+'?rand='+ Math.random().toString(32).substring(2) +'" alt="">';
-                thumb += '      </a>';
-                thumb += '     <h3>';
-                thumb += '         <a href="http://prezi.com/prezi/id/' + item.id + '">'+item.title+'</a>';
-                thumb += '       </h3>';
-                thumb += '      <p><a href="' + item.creator.profileUrl + '"><span>' + item.creator.name + '</span></a> | <span>'+item.createdAt+'</span></p>';
-                thumb += '  </div>';   
-                
-                if(i<3)
-                    $(".first_row").append(thumb); 
-                
-                if(i>=3 && i<6) 
-                    $(".second_row").append(thumb); 
-                
-                if(i>=6 && i<9) 
-                    $(".third_row").append(thumb); 
-            });
+                var prezis = JSON.parse(data.prezis);
+                $.each(prezis, function(i, item) {
+
+                    thumb  = '  <div class="col-md-4 portfolio-item">';
+                    thumb += '      <a href="http://prezi.com/prezi/id/' + item.id + '">';
+                    thumb += '         <img class="img-responsive" src="'+item.thumbnail+'?rand='+ Math.random().toString(32).substring(2) +'" alt="">';
+                    thumb += '      </a>';
+                    thumb += '     <h3>';
+                    thumb += '         <a href="http://prezi.com/prezi/id/' + item.id + '">'+item.title+'</a>';
+                    thumb += '       </h3>';
+                    thumb += '      <p><a href="' + item.creator.profileUrl + '"><span>' + item.creator.name + '</span></a> | <span>'+item.createdAt+'</span></p>';
+                    thumb += '  </div>';   
+
+                    if(i<3)
+                        $(".first_row").append(thumb); 
+
+                    if(i>=3 && i<6) 
+                        $(".second_row").append(thumb); 
+
+                    if(i>=6 && i<9) 
+                        $(".third_row").append(thumb); 
+                });
+            }
         }
     });
 })();
 
+
 $( ".search-box" ).keyup(function() {
     var text = $(this).val();
-    console.log(text);
+    
+    if(text) {
+        $.ajax({
+            url: "http://localhost:8080/search/" + text,
+            dataType: "json",    
+            context: document.body,
+            success: function(data) {
+
+                if(data.status == 'success') {
+
+                    $(".first_row").html('');
+                    $(".second_row").html('');
+                    $(".third_row").html('');
+
+                    var prezis = JSON.parse(data.prezis);
+
+                     $.each(prezis, function(i, item) {
+
+                        thumb  = '  <div class="col-md-4 portfolio-item">';
+                        thumb += '      <a href="http://prezi.com/prezi/id/' + item.id + '">';
+                        thumb += '         <img class="img-responsive" src="'+item.thumbnail+'?rand='+ Math.random().toString(32).substring(2) +'" alt="">';
+                        thumb += '      </a>';
+                        thumb += '     <h3>';
+                        thumb += '         <a href="http://prezi.com/prezi/id/' + item.id + '">'+item.title+'</a>';
+                        thumb += '       </h3>';
+                        thumb += '      <p><a href="' + item.creator.profileUrl + '"><span>' + item.creator.name + '</span></a> | <span>'+item.createdAt+'</span></p>';
+                        thumb += '  </div>';   
+
+                        if(i<3)
+                            $(".first_row").append(thumb); 
+
+                        if(i>=3 && i<6) 
+                            $(".second_row").append(thumb); 
+
+                        if(i>=6 && i<9) 
+                            $(".third_row").append(thumb); 
+                    });
+                }
+            }
+        }); 
+    }
 });
